899cb3289c64c84c79c8c8370186d8a2eb250276
Marissa Kawinski <marissakawinski@gmail.com>
2019-06-03 21:39:03 -0400

    Add hashing to stored passwords

--- src/routers/user.js
+++ src/routers/user.js
@@ -4,7 +4,6 @@ const router = new express.Router()
 
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
-
     try {
         await user.save()
         res.status(201).send(user)
@@ -48,7 +47,12 @@ router.patch('/users/:id', async (req, res) => {
     }
 
     try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
+        const user = await User.findById(req.params.id)
+        updates.forEach((update) => {
+            user[update] = req.body[update]
+        })
+        await user.save()
+        //const user = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true })
 
         if (!user) {
             return res.status(404).send()
